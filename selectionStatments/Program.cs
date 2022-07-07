a46c2223365dd835117522f6d30cdd16d2917cab
Tanner Van Amen <Tvanamen@yahoo.com>
2022-07-06 18:51:29 -0600

    inital commit

---
+++ selectionStatments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace selectionStatments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumber = new Random();
+            var favNumber = randomNumber.Next(1, 100);
+            Console.WriteLine("Please guess a number between 1 and 500");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Good guess you got it!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Guess was too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Guess was too high!");
+            }
+            if (userInput != favNumber)
+            {
+                Console.WriteLine("thanks for playing");
+            }
+
+
+        }
+    }
+}
